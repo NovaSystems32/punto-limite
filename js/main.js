c67d71e4bffc2985a61930769d947fd23461e358
Nova Systems <novasystems32@gmail.com>
2026-06-06 19:27:00 -0300

    Fix card: placeholder celeste cuando no hay imagen

--- js/main.js
+++ js/main.js
@@ -65,7 +65,9 @@ var pdCurrentImgIndex = 0;
 /* --- Render de una card de producto --- */
 function renderCard(data, docId) {
   var img    = data.img || '';
-  var imgSrc = (img.startsWith('http') || img.startsWith('data:')) ? img : 'img/' + img;
+  var imgSrc = img
+    ? ((img.startsWith('http') || img.startsWith('data:')) ? img : 'img/' + img)
+    : 'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'300\' height=\'360\' viewBox=\'0 0 300 360\'%3E%3Crect width=\'300\' height=\'360\' fill=\'%23C8DDEF\'/%3E%3Ctext x=\'50%25\' y=\'50%25\' dominant-baseline=\'middle\' text-anchor=\'middle\' font-family=\'sans-serif\' font-size=\'14\' fill=\'%23001F5B\'%3ESin imagen%3C/text%3E%3C/svg%3E';
   var stock = 99;
   if (data.stockMatrix && Object.keys(data.stockMatrix).length) {
     stock = Object.values(data.stockMatrix).reduce(function(a,b){ return a+(Number(b)||0); }, 0);
